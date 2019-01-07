5ee79d9f7b0507f995bd820d550dffff5a79a017
w91379137 <yc@spe3d.co>
2019-01-07 21:48:47 +0800

    顯示

--- src/class/view/Render.ts
+++ src/class/view/Render.ts
@@ -3,12 +3,30 @@ export class Render {
 
     constructor() {
 
+        let x_array = Array(20).fill(0).map((_, index) => index);
+        let y_array = Array(20).fill(0).map((_, index) => index);
+
+        x_array.forEach(x => {
+            y_array.forEach(y => {
+                this.createDiv(x, y);
+            });
+        });
+    }
+
+    createDiv(x, y) {
         let div = document.createElement('div');
         div.style.width = '10px';
         div.style.height = '10px';
         div.style.border = 'thin solid blue';
+
+        div.style.position = 'absolute';
+
+        div.style.left = '50%';
+        div.style.top = '50%';
+
+        div.style.marginLeft = (x * 10) + 'px';
+        div.style.marginTop = (y * 10) + 'px';
+
         document.querySelector('body').appendChild(div);
-
     }
-
 }
